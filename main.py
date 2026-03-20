d238fe4023b7d6d65f935a34ffd890731ac426d4
fontele442-cpu <fontele442@gmail.com>
2026-03-20 16:14:16 +0500

    Refactor bot and Flask server initialization

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import os
+import threading
+from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("8604215027:AAFHWgWwxMjHbehlQ5A7-1rR8mIgFCvXFUA")
+# --- Telegram bot ---
+TOKEN = "8604215027:AAFHWgWwxMjHbehlQ5A7-1rR8mIgFCvXFUA"
+bot_app = ApplicationBuilder().token(TOKEN).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot ishga tushdi ✅")
 
-app = ApplicationBuilder().token(TOKEN).build()
+bot_app.add_handler(CommandHandler("start", start))
 
-app.add_handler(CommandHandler("start", start))
+# --- Flask server ---
+web_app = Flask('')
 
-print("Bot ishga tushdi...")
-app.run_polling()
-
-from flask import Flask
-import threading
-
-app = Flask('')
-
-@app.route('/')
+@web_app.route('/')
 def home():
     return "Bot ishlayapti!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    web_app.run(host='0.0.0.0', port=8080)
 
 threading.Thread(target=run).start()
+
+# --- Botni ishga tushirish ---
+print("Bot ishga tushdi...")
+bot_app.run_polling()
